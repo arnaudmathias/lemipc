0d2af289da78b08847a184637e9b43821dd1dd72
indiedriver <amathias63@gmail.com>
2017-12-09 19:37:56 +0100

    init shared memory

--- include/lemipc.h
+++ include/lemipc.h
@@ -6,13 +6,21 @@
 /*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 18:31:40 by amathias          #+#    #+#             */
-/*   Updated: 2017/12/09 18:32:12 by amathias         ###   ########.fr       */
+/*   Updated: 2017/12/09 19:11:35 by amathias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMPIC_H
-#define LEMPIC_H
-
-
+# define LEMPIC_H
+# define BOARD_SIZE 16
+# include <sys/stat.h>
+# include <sys/msg.h>
+# include <sys/types.h>
+# include <sys/shm.h>
+# include <sys/ipc.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,35 @@
 /*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 18:32:27 by amathias          #+#    #+#             */
-/*   Updated: 2017/12/09 18:32:51 by amathias         ###   ########.fr       */
+/*   Updated: 2017/12/09 19:37:02 by amathias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemipc.h"
 
+void	init_shared_memory()
+{
+	key_t key;
+	int shmid;
+	char *data;
+
+	if (access("/tmp/ipc_board", F_OK) != 0) {
+		creat("/tmp/ipc_board", 0644);
+	}
+	key = ftok("/tmp/ipc_board", 'R');
+	if ((shmid = shmget(key, 1024, 0644 | IPC_CREAT)) < 0) {
+		perror("shmget");
+		exit(1);
+	}
+	if ((data = shmat(shmid, NULL, 0)) == (char *) -1) {
+		perror("shmat");
+		exit(1);
+	}
+	/* shmctl(shmid, IPC_RMID, NULL); // Destroy shared memory */
+}
+
 int main()
 {
-
+	init_shared_memory();
 	return 0;
 }
